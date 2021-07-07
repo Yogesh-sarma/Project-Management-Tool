af0d0cb61393aadf8646bf97af3431516aae4543
Yogesh Sarma <addankiyogesh@gmail.com>
2021-07-07 15:33:21 +0530

    persistence layer added

--- PPMTool/src/main/java/com/ppmtool/config/PPMToolConfig.java
+++ PPMTool/src/main/java/com/ppmtool/config/PPMToolConfig.java
@@ -1,13 +1,130 @@
 package com.ppmtool.config;
 
+import java.beans.PropertyVetoException;
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 
 @EnableWebMvc
 @Configuration
+@EnableTransactionManagement
 @ComponentScan(basePackages="com.ppmtool")
+@PropertySource({"classpath:my-sql.properties"})
 public class PPMToolConfig implements WebMvcConfigurer{
+	
+	@Autowired
+	private Environment env;
+	
+	@Bean
+	public DataSource myDataSource(){
+		
+		ComboPooledDataSource myDataSource=new ComboPooledDataSource();
+		
+		try{
+			myDataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+		}
+		catch(PropertyVetoException exc){
+			throw new RuntimeException(exc);			
+		}
+		
+		myDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+		myDataSource.setUser(env.getProperty("jdbc.user"));
+		myDataSource.setPassword(env.getProperty("jdbc.password"));
+		
+		myDataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+		myDataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
+		myDataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));		
+		myDataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
+		
+		return myDataSource;
+	}
+	
+	@Bean
+	public LocalSessionFactoryBean sessionFactory(){
+		LocalSessionFactoryBean sessionFactor = new LocalSessionFactoryBean();
+		
+		sessionFactor.setDataSource(myDataSource());
+		sessionFactor.setPackagesToScan(env.getProperty("hibernate.packagesToScan"));
+		sessionFactor.setHibernateProperties(getHibernateProperties());
+		
+		return sessionFactor;
+	}
+	
+	@Bean
+	@Autowired
+	public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+		
+		// setup transaction manager based on session factory
+		HibernateTransactionManager txManager = new HibernateTransactionManager();
+		txManager.setSessionFactory(sessionFactory);
 
+		return txManager;
+	}
+	
+	private Properties getHibernateProperties(){
+		Properties props= new Properties();
+		props.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
+		props.setProperty("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+		
+		return props;
+	}
+	
+	private int getIntProperty(String pname){
+		String propVal=env.getProperty(pname);
+		
+		int intPropVal=Integer.parseInt(propVal);
+		
+		return intPropVal;
+	}
+	
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+          .addResourceHandler("/resources/**")
+          .addResourceLocations("/resources/");
+    }
+	
+	@Bean
+    public ViewResolver viewResolver() {
+        
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        
+        viewResolver.setPrefix("/WEB-INF/view/");
+        viewResolver.setSuffix(".jsp");
+        
+        return viewResolver;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ PPMTool/src/main/java/com/ppmtool/services/ProjectService.java
@@ -0,0 +1,8 @@
+package com.ppmtool.services;
+
+import com.ppmtool.entities.Project;
+
+public interface ProjectService {
+	
+	public Project saveOrUpdate(Project project);
+}
